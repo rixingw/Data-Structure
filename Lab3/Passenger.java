01cd23ed23aef868673e81563b3a9d90c39635a8
Elvin Xu <elvinkx@users.noreply.github.com>
2016-02-16 14:44:54 -0500

    Update Passenger.java

--- Lab3/Passenger.java
+++ Lab3/Passenger.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-
-/**
-
-class Passage
-*/
-
-public class Passenger
-{
-	private int destination;
-
-	public Passenger(int currentStation)
-	{
-=======
 /**
  * Group 14:
  * Terrance Curley
@@ -28,22 +14,14 @@ public class Passenger{
     private int destination;
 
     public Passenger(int currentStation){
->>>>>>> 1870e94e3d6b44a1ca4cb2d0187b900c148cd435
 	//Set destination to (0 - 4) but not currentStaion
 
 
     }
 
-<<<<<<< HEAD
-	public int getDestination()
-	{
-		return destination;
-	}
-=======
     public int getDestination(){
         return destination;
     }
->>>>>>> 1870e94e3d6b44a1ca4cb2d0187b900c148cd435
 
 }
 
