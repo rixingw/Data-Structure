fe7ed91ac3209e66e2000ebc4e66b69fd2d3be73
Elvin Xu <elvinkx@users.noreply.github.com>
2016-02-17 09:10:49 -0500

    Update Passenger.java

--- Lab3/Passenger.java
+++ Lab3/Passenger.java
@@ -9,17 +9,36 @@
  * Due February 16th, 2016
  * 
  */
+import java.util.Random;
 
 public class Passenger{
     private int destination;
 
     public Passenger(int currentStation){
 	//Set destination to (0 - 4) but not currentStaion
-
-
+    //note a single Random object is reused here
+    	
+    	Random randomGenerator = new Random();
+	    for (int i = 0; i <= 100; i++){
+	      destination = randomGenerator.nextInt(4);
+	      while (destination != currentStation){
+	    	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
+		      getDestination();
+	      } 
+	      
+	      /*
+	      if (destination != currentStation){
+	    	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
+		      getDestination();
+	      }
+	      else
+	    	  destination = randomGenerator.nextInt(4);*/
+        }
+    	
     }
 
-    public int getDestination(){
+
+	public int getDestination(){
         return destination;
     }
 
