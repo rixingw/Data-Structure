76eb18c11edd947aa1c595b10efe8bc7a762957e
Elvin Xu <elvinkx@users.noreply.github.com>
2016-02-23 11:01:30 -0500

    Update Passenger.java

--- Lab3/Passenger.java
+++ Lab3/Passenger.java
@@ -19,28 +19,13 @@ public class Passenger{
     //note a single Random object is reused here
     	
     	int testDestination = getStation();
-    	while (testDestination != currentStation){
+    	while (testDestination == currentStation){
     		getStation();
     	}
     	
     	getDestination();
     	
-    	/*
-    	Random randomGenerator = new Random();
-	    for (int i = 0; i <= 100; i++){
-	      destination = randomGenerator.nextInt(4);
-	      while (destination != currentStation){
-	    	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
-		      getDestination();
-	      } */
-	      
-	      /*
-	      if (destination != currentStation){
-	    	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
-		      getDestination();
-	      }
-	      else
-	    	  destination = randomGenerator.nextInt(4);*/ 	
+    	
     }
     
     public int getStation(){
@@ -50,9 +35,25 @@ public class Passenger{
     }
 
 
-    public int getDestination(){
+	public int getDestination(){
         return destination * 5;
     }
 
 }
 
+/*
+Random randomGenerator = new Random();
+for (int i = 0; i <= 100; i++){
+  destination = randomGenerator.nextInt(4);
+  while (destination != currentStation){
+	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
+      getDestination();
+  } */
+  
+  /*
+  if (destination != currentStation){
+	  System.out.println("Passenger " + i + " 's destination goes to " + destination);
+      getDestination();
+  }
+  else
+	  destination = randomGenerator.nextInt(4);*/ 	
